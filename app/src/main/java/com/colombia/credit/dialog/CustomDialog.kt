b3c96cc913bf9f886d99d01dca8ec220aaf0701c
weishl <weishengli@guidefuture.com>
2023-04-19 16:06:47 +0800

    客服邮箱改为copy

--- app/src/main/java/com/colombia/credit/dialog/CustomDialog.kt
+++ app/src/main/java/com/colombia/credit/dialog/CustomDialog.kt
@@ -1,15 +1,15 @@
 package com.colombia.credit.dialog
 
 import android.content.Context
+import com.colombia.credit.R
 import com.colombia.credit.app.getAppContext
 import com.colombia.credit.databinding.DialogCutomBinding
-import com.colombia.credit.expand.getEmail
-import com.colombia.credit.expand.getServiceTel
-import com.colombia.credit.expand.getWhatsAppTel
+import com.colombia.credit.expand.*
 import com.colombia.credit.manager.Launch
 import com.common.lib.dialog.DefaultDialog
 import com.common.lib.expand.setBlockingOnClickListener
 import com.common.lib.viewbinding.binding
+import com.util.lib.copyFile
 import com.util.lib.ifShow
 
 class CustomDialog constructor(context: Context) : DefaultDialog(context) {
@@ -34,7 +34,9 @@ class CustomDialog constructor(context: Context) : DefaultDialog(context) {
             dismiss()
         }
         binding.etvEmail.setBlockingOnClickListener{
-            Launch.skipToEmail(getAppContext())
+            val email = getEmail()
+            copyClick(email)
+            toast(R.string.copy_success)
             dismiss()
         }
         binding.etvTelephone.setBlockingOnClickListener {
--- app/src/main/java/com/colombia/credit/expand/Util.kt
+++ app/src/main/java/com/colombia/credit/expand/Util.kt
@@ -1,6 +1,11 @@
 package com.colombia.credit.expand
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Build
+import com.colombia.credit.BuildConfig
+import com.colombia.credit.LoanApplication.Companion.getAppContext
 
 
 /**
@@ -8,4 +13,22 @@ import android.os.Build
  */
 inline fun isXiaomi(): Boolean {
     return Build.BRAND.toUpperCase() == "XIAOMI"
+}
+
+fun copyClick(copyText: String) {
+    if (copyText.isEmpty()) {
+        return
+    }
+    val myClipboard =
+        getAppContext()
+            .getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    val myClip = ClipData.newPlainText("text", copyText)
+    try {
+        myClipboard.setPrimaryClip(myClip)
+    } catch (e: SecurityException) {
+        if (BuildConfig.DEBUG) {
+            e.printStackTrace()
+            throw e
+        }
+    }
 }
